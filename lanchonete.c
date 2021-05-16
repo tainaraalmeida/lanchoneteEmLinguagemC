fa33173fff244cc7972cbbbb232b9748c844a47c
Tai <52765257+tainaraalmeida@users.noreply.github.com>
2021-05-16 17:20:02 -0300

    Update lanchonete.c

--- lanchonete.c
+++ lanchonete.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
+#include<stdio.h>
+#include<string.h>
+#include<locale.h>
+#include <ctype.h>
 
 int main(){
-	
+setlocale(LC_ALL, "");	
 
     int cod;
     int quant;
@@ -21,7 +22,7 @@ int main(){
     printf("                                    |  100:      Cachorro Quente   R$1.70 | \n");
     printf("                                    |  101:      Bauru Simples     R$2.30 |\n");
     printf("                                    |  102:      Bauro com ovo     R$2.60 | \n");
-    printf("                                    |  103:      Hamburguer        R$2.40 | \n");
+    printf("                                    |  103:      Hambúrguer        R$2.40 | \n");
     printf("                                    |  104: 	 Cheesburguer      R$2.50 | \n");
     printf("                                    |  105: 	 Refrigerante      R$1.00 | \n");
     printf("                                    |                                     | \n");
@@ -29,7 +30,7 @@ int main(){
      
 
 
-    printf(" 1) Digite o codigo do produto que deseja: \n");
+    printf(" 1) Digite o código do produto que deseja: \n");
     scanf("%i", &cod);
     printf(" 2) Qual a quantidade?: \n");
     scanf("%i", &quant);
@@ -39,31 +40,31 @@ int main(){
     {
     case 100:
        valor = (quant * 1.70);
-       printf("total a pagar sera: %f", valor);
+       printf("total a pagar será: %.2f", valor);
        break;
     case 101:
        valor = (quant * 2.30);
-       printf("total a pagar sera: %f", valor);
+       printf("total a pagar será: %.2f", valor);
        break;
     case 102:
        valor = (quant * 2.60);
-       printf("total a pagar sera: %f", valor);
+       printf("total a pagar será: %.2f", valor);
        break;
     case 103:
        valor = (quant * 2.40);
-       printf("total a pagar sera: %f", valor);
+       printf("total a pagar será: %.2f", valor);
        break; 
     case 104:
        valor = (quant * 2.50);
-       printf("total a pagar sera: %f", valor);
+       printf("total a pagar será: %.2f", valor);
        break; 
     case 105:
        valor = (quant * 1.00);
-       printf("total a pagar sera: %f", valor);
+       printf("total a pagar será:: %.2f", valor);
        break;
     
     default:
-        printf("Codigo Invalido");
+        printf("Código Inválido");
         break;
     }
     return 0;
